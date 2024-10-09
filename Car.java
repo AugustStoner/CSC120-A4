2bba612e8021545b7ddab7730ee4452ee86576b8
AugustStoner <jaas23@hampshire.edu>
2024-10-08 20:24:29 -0400

    Successful try/catch block for seatsRemaining method

--- Car.java
+++ Car.java
@@ -9,7 +9,6 @@ public class Car {
     public Car(int max) {
         this.maxCapacity = max;
         ArrayList<Passenger> passengersOnboard = new ArrayList<Passenger>(maxCapacity); // needs to be restricted by max capacity
-
     }
 
     /* Getters */
@@ -17,12 +16,20 @@ public class Car {
         return this.maxCapacity;
     }
     public int seatsRemaining() {
-        return (this.maxCapacity - passengersOnboard.size());
+        try {
+            return (this.maxCapacity - passengersOnboard.size());
+        } catch (Exception NullPointerException) {
+            System.out.println("There are no passengers in the car.");
+            return (this.maxCapacity);
+        }
+        //return (this.maxCapacity - passengersOnboard.size());
     }
 
 
     public static void main(String[] args) {
-        
+        Car myCar = new Car(100);
+        System.out.println("This car has " + myCar.getCapacity() + " total seats.");
+        System.out.println("This car has " + myCar.seatsRemaining() + " seats remaining.");
 
 
     }
